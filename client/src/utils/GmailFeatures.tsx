d07e1ff529fa3c7b5f9a06f47872779a1000da87
henryman1234 <henrynomo68@gmail.com>
2025-09-30 02:00:06 +0200

    Seventh Commit

--- client/src/utils/GmailFeatures.tsx
+++ client/src/utils/GmailFeatures.tsx
@@ -1,96 +1,96 @@
- { useEffect, useState } from "react";
+//  { useEffect, useState } from "react";
 
-const initialEmails =  [
-    {id: 1, title: "Apprendre le code", body: "React est un framework JS"},
-    {id: 2, title: "Apprendre Vue", body: "Vue est un framework JS"},
-    {id: 3, title: "Apprendre Angular", body: "Angular est un framework JS"},
-    {id: 4, title: "Apprendre Laravel", body: "Laravel est un framework PHP"},
-]
+// const initialEmails =  [
+//     {id: 1, title: "Apprendre le code", body: "React est un framework JS"},
+//     {id: 2, title: "Apprendre Vue", body: "Vue est un framework JS"},
+//     {id: 3, title: "Apprendre Angular", body: "Angular est un framework JS"},
+//     {id: 4, title: "Apprendre Laravel", body: "Laravel est un framework PHP"},
+// ]
 
-export function GMAILFeature () {
+// export function GMAILFeature () {
 
-    interface Message {
-        id: number,
-        title: string,
-        body: string
-    }
+//     interface Message {
+//         id: number,
+//         title: string,
+//         body: string
+//     }
 
-    const [emails, setEmails] = useState<Message[]>(initialEmails)
-    const [deletedEmail, setDeletedEmail] = useState<Message | null>(null)
-    const [showUndo, setShowUndo] = useState<boolean>(false)
-    const [undoTimer, setUndoTimer] = useState(5)
+//     const [emails, setEmails] = useState<Message[]>(initialEmails)
+//     const [deletedEmail, setDeletedEmail] = useState<Message | null>(null)
+//     const [showUndo, setShowUndo] = useState<boolean>(false)
+//     const [undoTimer, setUndoTimer] = useState(5)
 
-    const handleDelete = function (email:Message) {
-        setDeletedEmail(email)
-        setEmails(function(prev) {
-            return prev.filter(function(item: Message){
-                return item.id !== email.id
-            })
-        })
-        setUndoTimer(5)
-        setShowUndo(true)
-    }
+//     const handleDelete = function (email:Message) {
+//         setDeletedEmail(email)
+//         setEmails(function(prev) {
+//             return prev.filter(function(item: Message){
+//                 return item.id !== email.id
+//             })
+//         })
+//         setUndoTimer(5)
+//         setShowUndo(true)
+//     }
 
-    useEffect(function(){
-        if (showUndo && deletedEmail ) {
+//     useEffect(function(){
+//         if (showUndo && deletedEmail ) {
 
-            const timer = setInterval(function(){
-                setUndoTimer(function(prev:number) {
-                   if (prev <= 1) {
-                        clearInterval(timer)
-                        setDeletedEmail(null)
-                        setShowUndo(false)
-                        return 0
-                   }
-                    return prev - 1
-                })
+//             const timer = setInterval(function(){
+//                 setUndoTimer(function(prev:number) {
+//                    if (prev <= 1) {
+//                         clearInterval(timer)
+//                         setDeletedEmail(null)
+//                         setShowUndo(false)
+//                         return 0
+//                    }
+//                     return prev - 1
+//                 })
     
-            }, 1000)
+//             }, 1000)
     
-            return function () {
-                clearInterval(timer)
-            }
-        } 
+//             return function () {
+//                 clearInterval(timer)
+//             }
+//         } 
         
-    }, [showUndo, deletedEmail])
+//     }, [showUndo, deletedEmail])
 
 
-    const handleUndo  = function () {
-        // if (undoTimer) {
-        //     clearInterval(undoTimer)
-        // }
-        if (deletedEmail) {
-            setEmails(function(prev: Message[]) {
-                return [deletedEmail, ...prev]
-            })
-        }
-        setDeletedEmail(null)
-        setShowUndo(false)
-    }
+//     const handleUndo  = function () {
+//         // if (undoTimer) {
+//         //     clearInterval(undoTimer)
+//         // }
+//         if (deletedEmail) {
+//             setEmails(function(prev: Message[]) {
+//                 return [deletedEmail, ...prev]
+//             })
+//         }
+//         setDeletedEmail(null)
+//         setShowUndo(false)
+//     }
 
-    return (
-        <div className="container" style={{position: "relative"}}>
-            {emails.map(function(email){
-                return (
-                    <article key={email.id} className="email">
-                        <div className="emailBody">
-                            <h2>{email.title}</h2>
-                            <span>{email.body}</span>
-                        </div>
-                        <div className="emailBottom">
-                            <button   className="undoButton" >Undo</button>
-                            <button onClick={function() {
-                                handleDelete(email)
-                            }} className="deleteButton" >Delete</button>
-                        </div>
-                    </article>
-                )
-            })}
-            {showUndo && <div className="delete">
-                Annuler la supprésion
-                <span>{undoTimer}</span>
-                <button onClick={handleUndo}>Annuler</button>
-            </div>}
-        </div>
-    )
-}
+//     return (
+//         <div className="container" style={{position: "relative"}}>
+//             {emails.map(function(email){
+//                 return (
+//                     <article key={email.id} className="email">
+//                         <div className="emailBody">
+//                             <h2>{email.title}</h2>
+//                             <span>{email.body}</span>
+//                         </div>
+//                         <div className="emailBottom">
+//                             <button   className="undoButton" >Undo</button>
+//                             <button onClick={function() {
+//                                 handleDelete(email)
+//                             }} className="deleteButton" >Delete</button>
+//                         </div>
+//                     </article>
+//                 )
+//             })}
+//             {showUndo && <div className="delete">
+//                 Annuler la supprésion
+//                 <span>{undoTimer}</span>
+//                 <button onClick={handleUndo}>Annuler</button>
+//             </div>}
+//         </div>
+//     )
+// }
