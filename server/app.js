725ebbfc9b13d1381949b167a073719eaf458bab
Henry Euloge <162862779+henryman1234@users.noreply.github.com>
2025-09-30 12:35:07 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://kingtech-front.onrender.com"
+    origin: "*"
     methods:"GET, PUT, DELETE, POST, PATCH",
     credentials: true,
     optionsSuccessStatus: 204,
